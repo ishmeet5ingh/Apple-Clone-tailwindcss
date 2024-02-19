286152514b42c4644b3852049889c4d9f6d0975d
ishmeet singh <ishmeet134@gmail.com>
2024-02-19 08:36:49 +0530

    created

---
+++ devices.js
@@ -0,0 +1,40 @@
+const devices = [
+    {
+        img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/store-card-13-mac-nav-202310?wid=400&hei=260&fmt=png-alpha&.v=1696964122666",
+        heading: "Mac"
+    },
+    {
+        img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/store-card-13-iphone-nav-202309_GEO_EMEA?wid=400&hei=260&fmt=png-alpha&.v=1692971740071",
+        heading: "iPhone"
+    },
+    {
+        img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/store-card-13-ipad-nav-202210?wid=400&hei=260&fmt=png-alpha&.v=1664912135437",
+        heading: "iPad"
+    },
+    {
+        img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/store-card-13-watch-nav-202309_GEO_IN?wid=400&hei=260&fmt=png-alpha&.v=1693703814407",
+        heading: "Apple Watch"
+    },
+    {
+        img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/store-card-13-airpods-nav-202209?wid=400&hei=260&fmt=png-alpha&.v=1660676485885",
+        heading: "AirPods"
+    },
+    {
+        img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/store-card-13-airtags-nav-202108?wid=400&hei=260&fmt=png-alpha&.v=1625783380000",
+        heading: "AirTag"
+    },
+    {
+        img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/store-card-13-appletv-nav-202210?wid=400&hei=260&fmt=png-alpha&.v=1664628458484",
+        heading: "Apple Tv 4K"
+    },
+    {
+        img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/store-card-13-homepod-nav-202301?wid=400&hei=260&fmt=png-alpha&.v=1670389216654",
+        heading: "HomePod"
+    },
+    {
+        img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/store-card-13-accessories-nav-202309?wid=400&hei=260&fmt=png-alpha&.v=1692803114952",
+        heading: "Accessories"
+    },
+]
+
+export { devices };
---
+++ index.js
@@ -0,0 +1,72 @@
+import { devices } from './devices.js';
+
+const devs = document.querySelector(".devices");
+
+
+devices.forEach(item =>{
+    var div = document.createElement('div');
+    var imgElement = document.createElement('img');
+    var heading = document.createElement('h4');
+    imgElement.src = item.img;
+    heading.innerHTML = item.heading;
+    imgElement.classList.add("w-32",)
+    div.appendChild(imgElement)
+    div.appendChild(heading)
+    div.classList.add("pr-4", "md:pr-10", "inline-block", )
+    devs.appendChild(div)
+})
+
+// devs.lastChild.classList.add('pr-10','md:pr-20')
+
+document.addEventListener('DOMContentLoaded', function () {
+  const animatedDiv = document.getElementById('animatedDiv');
+  const overlay = document.querySelector('.overlay');
+  const slider = document.querySelectorAll('.slider');
+  const iTag = document.querySelectorAll('.iTag');
+  const i1 = document.querySelector('.fa-chevron-left');
+  const i2 = document.querySelector('.fa-chevron-right');
+  var counter = 0;
+
+  slider.forEach((slide, index) =>{
+    slide.style.left = `${index * 100}%`
+});
+
+i1.addEventListener("click", function(){
+    counter = (counter-1+slider.length)%slider.length
+    slideImage();
+    console.log(counter)
+})
+i2.addEventListener("click", function(){
+    counter = (counter+1)%slider.length
+    slideImage();
+    console.log(counter)
+})
+
+
+const slideImage = () => {
+    slider.forEach((slide) => {
+        slide.style.transform = `translateX(-${counter * 100}%)`
+    });
+}
+
+  // Trigger the animation on page load
+  setTimeout(() => {
+        animatedDiv.classList.remove('opacity-0', 'translate-y-[-150%]');
+        animatedDiv.classList.add('opacity-100', 'translate-y-0');
+    }, 100);
+    setTimeout(() => {
+        slider.forEach(slider => {
+            slider.classList.remove('text-white');
+            slider.classList.add('text-[#848484]');
+        }); 
+
+        overlay.classList.remove('bg-[#0171E3]');
+        overlay.classList.add('bg-white');
+
+        iTag.forEach(iTag => {
+            iTag.classList.remove('text-[#0171E3]');
+            iTag.classList.add('text-[#848484]');
+        }); 
+    }, 1000);
+});
+
